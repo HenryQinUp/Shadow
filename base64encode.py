0ae48e9272dc683d58d5eadda99f86665a250bcf
HenryQinUp <51472137+HenryQinUp@users.noreply.github.com>
2022-08-07 01:23:43 +0800

    Update base64encode.py

--- base64encode.py
+++ base64encode.py
@@ -3,8 +3,8 @@ from Cryptodome.Cipher import AES
 
 with open("jiedian.txt") as f:
     data = f.read() #需要加密的内容
-data1 = data.decode('utf8')
+data1 = data.encode('utf8')
 code = base64.b64encode(data1)
-data2 = code.encode('utf8')
+data2 = code.decode('utf8')
 with open("jiedian.txt", "w") as out_file:
     out_file.write(data2)
